345f5d3ff6fc043aadde53a34c8f5f72e8af946c
mateusanjost <mateusanjosmgb@gmail.com>
2022-09-11 01:11:37 -0300

    up

--- src/components/box/index.tsx
+++ src/components/box/index.tsx
@@ -1,5 +1,7 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
+import { useModal } from "../../hooks/useModal";
 import { ImpulseListProps } from "../../types/impuse";
+import { ConfirmationModal } from "../modal/confirmation";
 import styles from "./Box.module.scss";
 
 type BoxProps = ImpulseListProps & {
@@ -14,19 +16,43 @@ const Box = ({
   backgroundColor,
 }: BoxProps) => {
   return (
-    <div
-      key={id}
-      className={styles.item}
-      style={{
-        backgroundColor: backgroundColor,
-      }}
-    >
-      <div className={styles.name}>{name}</div>
-      <div className={styles.description}>{details?.description}</div>
-      <div className={styles.button} onClick={() => handleRemove(id)}>
-        -
+    <>
+      <div
+        key={id}
+        className={styles.item}
+        style={{
+          backgroundColor: backgroundColor,
+        }}
+      >
+        <div className={styles.name}>{name}</div>
+        <div className={styles.description}>{details?.description}</div>
+        <div className={styles.button} onClick={() => handleRemove(id)}>
+          -
+        </div>
       </div>
-    </div>
+    </>
+  );
+};
+
+const Modal = () => {
+  const { isShown, toggle } = useModal();
+
+  const onConfirm = () => toggle();
+  const onCancel = () => toggle();
+
+  return (
+    <Modal
+      isShown={isShown}
+      hide={toggle}
+      headerText="Confirmation"
+      modalContent={
+        <ConfirmationModal
+          onConfirm={onConfirm}
+          onCancel={onCancel}
+          message="Are you sure you want to delete element?"
+        />
+      }
+    />
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,9 @@
 import Footer from "../components/footer";
 import Header from "../components/header";
 import Impulse from "../components/Impulse";
-import { Modal } from "../components/modal";
-import { ConfirmationModal } from "../components/modal/confirmation";
-import { useModal } from "../hooks/useModal";
 import styles from "../styles/index.module.scss";
 
 const Index = () => {
-  // const { isShown, toggle } = useModal();
-
-  // const onConfirm = () => toggle();
-  // const onCancel = () => toggle();
-
   return (
     <div className={styles.body}>
       <Header />
